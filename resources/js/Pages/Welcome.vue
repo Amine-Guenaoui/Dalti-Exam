b173641a5b643edcb4bf9ee2bea0d66134a45bc5
Amine Guenaoui <44202091+Amine-Guenaoui@users.noreply.github.com>
2025-03-02 12:22:05 +0000

    first commit

---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <h1>Hello, Inertia.js with Vue!</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Welcome',
+};
+</script>
---
+++ resources/views/app.blade.php
@@ -0,0 +1,14 @@
+<!-- filepath: resources/views/app.blade.php -->
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Laravel</title>
+    @vite('resources/css/app.css')
+</head>
+<body>
+    @inertia
+    @vite('resources/js/app.js')
+</body>
+</html>
